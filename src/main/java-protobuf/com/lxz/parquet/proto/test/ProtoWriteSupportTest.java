36d9c3374c331b9c734408fbde53e630b1d52e4b
郑晓洛 <zhengxiaoluo@haizhi.com>
2016-04-28 11:47:08 +0800

    protobuf test

---
+++ src/main/java-protobuf/com/lxz/parquet/proto/test/ProtoWriteSupportTest.java
@@ -0,0 +1,13 @@
+package com.lxz.parquet.proto.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by xiaolezheng on 16/4/28.
+ */
+public class ProtoWriteSupportTest {
+    private static final Logger logger = LoggerFactory.getLogger(ProtoWriteSupportTest.class);
+
+    
+}
